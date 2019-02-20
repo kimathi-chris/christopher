f05a83f953c71148fd6f63a9bda869d566d787da
kimathi-chris <kimathichris13@gmail.com>
2019-02-20 17:46:07 +0300

    This are test changes

--- script5.sh
+++ script5.sh
@@ -4,7 +4,7 @@ testFunction(){ echo "Lewis Test";}
 testFunction2(){ echo "Chris Test";}
 testFunction3(){ echo "Hello chris";}
 testFunction4(){ echo "Hello C";}
-
+testFunction6(){ echo "Hello java java";}
 #Expose commands to handle the app
 case "$1" in
 testFunction)
@@ -18,6 +18,12 @@ testFunction3)
 	  
 testFunction4)
       testFunction4;;
+	  
+testFunction4)
+      testFunction4;;
+testFunction6)
+      testFunction6;;	  
+	  
 *)
 
 echo "Usage: $0 {testFunction}"
