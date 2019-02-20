dd09421e88350fc1434d44ad322ac46b163f5868
kimathi-chris <kimathichris13@gmail.com>
2019-02-20 12:46:05 +0300

    Added test function 4

--- script5.sh
+++ script5.sh
@@ -3,6 +3,7 @@
 testFunction(){ echo "Lewis Test";}
 testFunction2(){ echo "Chris Test";}
 testFunction3(){ echo "Hello chris";}
+testFunction4(){ echo "Hello C";}
 
 #Expose commands to handle the app
 case "$1" in
@@ -14,6 +15,9 @@ testFunction2)
 
 testFunction3)
       testFunction3;;
+	  
+testFunction4)
+      testFunction4;;
 *)
 
 echo "Usage: $0 {testFunction}"
