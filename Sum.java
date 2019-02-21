ca3ffa0c111344ec182d32591c4acc3116190c63
kimathi-chris <kimathichris13@gmail.com>
2019-02-21 11:01:33 +0300

    minor change in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Sum{
 public static void main(String[] args){
-int a,b,add;
+int a,b,c,add;
 
 Scanner buf = new Scanner(System.in);
 System.out.println("Enter the first number:");
@@ -10,7 +10,10 @@ a = buf.nextInt();
 System.out.println("Enter the second number:");
 b = buf.nextInt();
 
-add = a+b;
+System.out.println("Enter the third number:");
+b = buf.nextInt();
+
+add = a+b+c;
 System.out.println("Sum of the integers is:"  + add);
 }
 }
