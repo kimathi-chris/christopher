6c212b0688c6c1576abe8c72d97c5d9a9acb97eb
kimathi-chris <kimathichris13@gmail.com>
2019-02-21 10:25:03 +0300

    changed sum.sh

--- sum.sh
+++ sum.sh
@@ -9,7 +9,10 @@ read num2
 echo -n "Enter the third number: "
 read num3
 
-sum=`expr $num1 + $num2 + $num3`
+echo -n "Enter the fourth number: "
+read num4
+
+sum=`expr $num1 + $num2 + $num3 + $num4`
 echo "sum is "$sum
 #echo $sum
 
