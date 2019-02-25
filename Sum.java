037ab1008cff9a0b11c74b696b3447c7d53986fd
kimathi-chris <kimathichris13@gmail.com>
2019-02-25 11:32:53 +0300

    minor change

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Sum{
 public static void main(String[] args){
-int a,b,c,d,e,product;
+int a,b,c,d,product;
 
 Scanner buf = new Scanner(System.in);
 System.out.println("Enter the first number:");
@@ -16,12 +16,9 @@ c = buf.nextInt();
 System.out.println("Enter the fourth number:");
 d = buf.nextInt();
 
-System.out.println("Enter the fifth number:");
-e = buf.nextInt();
 
 
-
-product = a*b*c*d*e;
+product = a*b*c*d;
 System.out.println("Product of the integers is:"  + product);
 }
 }
